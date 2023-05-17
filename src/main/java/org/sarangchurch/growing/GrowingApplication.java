44c167b3046fef7f6d5fab85964d7d0c6bb065b1
sanguk <ocean@monymony.co>
2023-05-17 21:29:50 +0900

    Initial Commit

---
+++ src/main/java/org/sarangchurch/growing/GrowingApplication.java
@@ -0,0 +1,13 @@
+package org.sarangchurch.growing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sarangchurch/growing/GrowingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sarangchurch.growing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
