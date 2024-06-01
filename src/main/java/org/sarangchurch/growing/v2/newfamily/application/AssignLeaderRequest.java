56c5eb293cd78ed85fec19ea850d222f7b24b307
sanguk <ocean@monymony.co>
2024-06-01 20:48:01 +0900

    feat(새가족): 새가족 순장 등록 API

---
+++ src/main/java/org/sarangchurch/growing/v2/newfamily/application/AssignLeaderRequest.java
@@ -0,0 +1,10 @@
+package org.sarangchurch.growing.v2.newfamily.application;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class AssignLeaderRequest {
+    private Long newFamilyLeaderId;
+}
---
+++ src/main/java/org/sarangchurch/growing/v2/newfamily/presentation/AssignLeaderController.java
@@ -0,0 +1,23 @@
+package org.sarangchurch.growing.v2.newfamily.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.sarangchurch.growing.v2.newfamily.application.AssignLeaderRequest;
+import org.sarangchurch.growing.v2.newfamily.application.AssignLeaderService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class AssignLeaderController {
+
+    private final AssignLeaderService assignLeaderService;
+
+    @PostMapping("/api/v2/new-families/{newFamilyId}/assignNewFamilyLeader")
+    public void assignLeader(@PathVariable Long newFamilyId, @RequestBody @Valid AssignLeaderRequest request) {
+        assignLeaderService.assign(newFamilyId, request.getNewFamilyLeaderId());
+    }
+}
