4e980fdc2a65b1539670deaa0de6e15c5511dbaa
imcool2551 <35341221+imcool2551@users.noreply.github.com>
2024-06-20 20:56:01 +0900

    feat(새가족): 새가족 목록 조회 API 확장 (#30)

--- src/main/java/org/sarangchurch/growing/v2/feat/newfamily/query/NewFamilyQueryRepository.java
+++ src/main/java/org/sarangchurch/growing/v2/feat/newfamily/query/NewFamilyQueryRepository.java
@@ -3,6 +3,8 @@ package org.sarangchurch.growing.v2.feat.newfamily.query;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.sarangchurch.growing.v2.feat.term.domain.smallgroup.QSmallGroup;
+import org.sarangchurch.growing.v2.feat.term.domain.smallgroupleader.QSmallGroupLeader;
 import org.sarangchurch.growing.v2.feat.user.domain.QUser;
 import org.springframework.stereotype.Repository;
 
@@ -11,8 +13,7 @@ import java.util.List;
 import static org.sarangchurch.growing.v2.feat.newfamily.domain.newfamily.QNewFamily.newFamily;
 import static org.sarangchurch.growing.v2.feat.newfamily.domain.newfamilygroup.QNewFamilyGroup.newFamilyGroup;
 import static org.sarangchurch.growing.v2.feat.newfamily.domain.newfamilygroupleader.QNewFamilyGroupLeader.newFamilyGroupLeader;
-import static org.sarangchurch.growing.v2.feat.term.domain.smallgroup.QSmallGroup.smallGroup;
-import static org.sarangchurch.growing.v2.feat.term.domain.smallgroupleader.QSmallGroupLeader.smallGroupLeader;
+import static org.sarangchurch.growing.v2.feat.newfamily.domain.newfamilypromotelog.QNewFamilyPromoteLog.newFamilyPromoteLog;
 import static org.sarangchurch.growing.v2.feat.user.domain.QUser.user;
 
 @Repository
@@ -21,11 +22,21 @@ public class NewFamilyQueryRepository {
     private final JPAQueryFactory queryFactory;
 
     public List<NewFamily> findAll() {
+        // 새가족반
         QUser newFamilyGroupLeaderUser = new QUser("newFamilyGroupLeaderUser");
+
+        // 새가족일때 일반 순모임
+        QSmallGroup smallGroup = new QSmallGroup("smallGroup");
+        QSmallGroupLeader smallGroupLeader = new QSmallGroupLeader("smallGroupLeader");
         QUser smallGroupLeaderUser = new QUser("smallGroupLeaderUser");
 
+        // 등반한 순모임
+        QSmallGroup promotedSmallGroup = new QSmallGroup("promotedSmallGroup");
+        QSmallGroupLeader promotedSmallGroupLeader = new QSmallGroupLeader("promotedSmallGroupLeader");
+        QUser promotedSmallGroupLeaderUser = new QUser("promotedSmallGroupLeaderUser");
+
         return queryFactory.select(Projections.constructor(NewFamily.class,
-                        user.id,
+                        newFamily.id,
                         user.name,
                         user.phoneNumber,
                         user.birth,
@@ -34,16 +45,24 @@ public class NewFamilyQueryRepository {
                         newFamily.visitDate,
                         newFamily.etc,
                         newFamilyGroupLeaderUser.name,
-                        smallGroupLeaderUser.name
+                        smallGroupLeaderUser.name,
+                        promotedSmallGroupLeaderUser.name
                 ))
                 .from(newFamily)
                 .join(user).on(user.id.eq(newFamily.userId))
+                // 새가족반
                 .leftJoin(newFamilyGroup).on(newFamilyGroup.id.eq(newFamily.newFamilyGroupId))
                 .leftJoin(newFamilyGroupLeader).on(newFamilyGroupLeader.id.eq(newFamilyGroup.newFamilyGroupLeaderId))
                 .leftJoin(newFamilyGroupLeaderUser).on(newFamilyGroupLeaderUser.id.eq(newFamilyGroupLeader.userId))
+                // 새가족일때 일반 순모임
                 .leftJoin(smallGroup).on(smallGroup.id.eq(newFamily.smallGroupId))
                 .leftJoin(smallGroupLeader).on(smallGroupLeader.id.eq(smallGroup.smallGroupLeaderId))
                 .leftJoin(smallGroupLeaderUser).on(smallGroupLeaderUser.id.eq(smallGroupLeader.userId))
+                // 등반한 순모임
+                .leftJoin(newFamilyPromoteLog).on(newFamilyPromoteLog.id.eq(newFamily.newFamilyPromoteLogId))
+                .leftJoin(promotedSmallGroup).on(promotedSmallGroup.id.eq(newFamilyPromoteLog.smallGroupId))
+                .leftJoin(promotedSmallGroupLeader).on(promotedSmallGroupLeader.id.eq(promotedSmallGroup.smallGroupLeaderId))
+                .leftJoin(promotedSmallGroupLeaderUser).on(promotedSmallGroupLeaderUser.id.eq(promotedSmallGroupLeader.userId))
                 .fetch();
     }
 }
