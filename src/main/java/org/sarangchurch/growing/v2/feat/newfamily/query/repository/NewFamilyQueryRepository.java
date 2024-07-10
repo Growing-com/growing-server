a4c461eb9522fd6bc5bb91542df9153538a04d9d
sanguk <ocean@monymony.co>
2024-07-10 23:05:54 +0900

    feat: new famil get api

--- src/main/java/org/sarangchurch/growing/v2/feat/newfamily/query/repository/NewFamilyQueryRepository.java
+++ src/main/java/org/sarangchurch/growing/v2/feat/newfamily/query/repository/NewFamilyQueryRepository.java
@@ -23,6 +23,52 @@ import static org.sarangchurch.growing.v2.feat.user.domain.QUser.user;
 public class NewFamilyQueryRepository {
     private final JPAQueryFactory queryFactory;
 
+    public NewFamily findById(Long newFamilyId) {
+        // 새가족반
+        QUser newFamilyGroupLeaderUser = new QUser("newFamilyGroupLeaderUser");
+
+        // 새가족일때 일반 순모임
+        QSmallGroup smallGroup = new QSmallGroup("smallGroup");
+        QSmallGroupLeader smallGroupLeader = new QSmallGroupLeader("smallGroupLeader");
+        QUser smallGroupLeaderUser = new QUser("smallGroupLeaderUser");
+
+        // 등반한 순모임
+        QSmallGroup promotedSmallGroup = new QSmallGroup("promotedSmallGroup");
+        QSmallGroupLeader promotedSmallGroupLeader = new QSmallGroupLeader("promotedSmallGroupLeader");
+        QUser promotedSmallGroupLeaderUser = new QUser("promotedSmallGroupLeaderUser");
+
+        return queryFactory.select(Projections.constructor(NewFamily.class,
+                        newFamily.id,
+                        user.name,
+                        user.phoneNumber,
+                        user.birth,
+                        user.sex,
+                        user.grade,
+                        newFamily.visitDate,
+                        newFamily.etc,
+                        newFamilyGroupLeaderUser.name,
+                        smallGroupLeaderUser.name,
+                        promotedSmallGroupLeaderUser.name,
+                        newFamilyPromoteLog.promoteDate
+                ))
+                .from(newFamily)
+                .join(user).on(user.id.eq(newFamily.userId), newFamily.id.eq(newFamilyId))
+                // 새가족반
+                .leftJoin(newFamilyGroup).on(newFamilyGroup.id.eq(newFamily.newFamilyGroupId))
+                .leftJoin(newFamilyGroupLeader).on(newFamilyGroupLeader.id.eq(newFamilyGroup.newFamilyGroupLeaderId))
+                .leftJoin(newFamilyGroupLeaderUser).on(newFamilyGroupLeaderUser.id.eq(newFamilyGroupLeader.userId))
+                // 새가족일때 일반 순모임
+                .leftJoin(smallGroup).on(smallGroup.id.eq(newFamily.smallGroupId))
+                .leftJoin(smallGroupLeader).on(smallGroupLeader.id.eq(smallGroup.smallGroupLeaderId))
+                .leftJoin(smallGroupLeaderUser).on(smallGroupLeaderUser.id.eq(smallGroupLeader.userId))
+                // 등반한 순모임
+                .leftJoin(newFamilyPromoteLog).on(newFamilyPromoteLog.id.eq(newFamily.newFamilyPromoteLogId))
+                .leftJoin(promotedSmallGroup).on(promotedSmallGroup.id.eq(newFamilyPromoteLog.smallGroupId))
+                .leftJoin(promotedSmallGroupLeader).on(promotedSmallGroupLeader.id.eq(promotedSmallGroup.smallGroupLeaderId))
+                .leftJoin(promotedSmallGroupLeaderUser).on(promotedSmallGroupLeaderUser.id.eq(promotedSmallGroupLeader.userId))
+                .fetchOne();
+    }
+
     public List<NewFamily> findAll() {
         // 새가족반
         QUser newFamilyGroupLeaderUser = new QUser("newFamilyGroupLeaderUser");
