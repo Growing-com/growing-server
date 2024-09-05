5d6f4ce1ba98e958d79a623b6f823f4aaa587bd0
imcool2551 <35341221+imcool2551@users.noreply.github.com>
2024-09-05 13:05:17 +0900

    feat(new-fam): v1 query new fam single (#47)

--- src/main/java/org/sarangchurch/growing/v1/feat/newfamily/query/repository/V1NewFamilyQueryRepository.java
+++ src/main/java/org/sarangchurch/growing/v1/feat/newfamily/query/repository/V1NewFamilyQueryRepository.java
@@ -1,8 +1,10 @@
-package org.sarangchurch.growing.v1.feat.newfamily.query;
+package org.sarangchurch.growing.v1.feat.newfamily.query.repository;
 
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.sarangchurch.growing.v1.feat.newfamily.query.model.V1NewFamily;
+import org.sarangchurch.growing.v1.feat.newfamily.query.model.V1NewFamilyListItem;
 import org.sarangchurch.growing.v2.feat.user.domain.QUser;
 import org.springframework.stereotype.Repository;
 
@@ -21,13 +23,13 @@ import static org.sarangchurch.growing.v2.feat.user.domain.QUser.user;
 public class V1NewFamilyQueryRepository {
     private final JPAQueryFactory queryFactory;
 
-    public List<V1NewFamily> findAll() {
+    public List<V1NewFamilyListItem> findAll() {
         // 새가족반 리더 지체
         QUser newFamilyGroupLeaderUser = new QUser("newFamilyGroupLeaderUser");
         // 일반 순모임 리더 지체
         QUser smallGroupLeaderUser = new QUser("smallGroupLeaderUser");
 
-        return queryFactory.select(Projections.constructor(V1NewFamily.class,
+        return queryFactory.select(Projections.constructor(V1NewFamilyListItem.class,
                         newFamily.id.as("newFamilyId"),
                         user.name.as("name"),
                         user.sex.as("sex"),
@@ -54,5 +56,23 @@ public class V1NewFamilyQueryRepository {
                 // 정렬
                 .orderBy(newFamily.visitDate.desc())
                 .fetch();
+    }
+
+    public V1NewFamily findById(Long newFamilyId) {
+        return queryFactory.select(Projections.constructor(V1NewFamily.class,
+                        newFamily.id.as("newFamilyId"),
+                        user.name.as("name"),
+                        user.sex.as("sex"),
+                        user.phoneNumber.as("phoneNumber"),
+                        user.birth.as("birth"),
+                        newFamily.visitDate.as("visitDate"),
+                        user.grade.as("grade"),
+                        newFamily.etc.as("etc"),
+                        newFamilyGroup.id.as("newFamilyGroupId")
+                ))
+                .from(newFamily)
+                .join(user).on(user.id.eq(newFamily.userId), newFamily.id.eq(newFamilyId))
+                .leftJoin(newFamilyGroup).on(newFamilyGroup.id.eq(newFamily.newFamilyGroupId))
+                .fetchOne();
     }
 }
