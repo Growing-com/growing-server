f07ebef5674a55da565cc1f33ec8486f8615ce7a
sanguk <imcool2551@gmail.com>
2024-10-22 12:23:13 +0900

    feat: query users

--- src/main/java/org/sarangchurch/growing/v1/feat/term/domain/Duty.java
+++ src/main/java/org/sarangchurch/growing/v1/feat/term/domain/Duty.java
@@ -3,13 +3,10 @@ package org.sarangchurch.growing.v1.feat.term.domain;
 public enum Duty {
     PASTOR,
     CODY,
-
     SMALL_GROUP_LEADER,
     NEW_FAMILY_GROUP_LEADER,
-
     SMALL_GROUP_MEMBER,
     NEW_FAMILY_GROUP_MEMBER,
-
     NEW_FAMILY,
     NOT_PLACED;
 }
---
+++ src/main/java/org/sarangchurch/growing/v1/feat/user/query/model/UserListItem.java
@@ -0,0 +1,21 @@
+package org.sarangchurch.growing.v1.feat.user.query.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.sarangchurch.growing.core.interfaces.common.Sex;
+import org.sarangchurch.growing.v1.feat.term.domain.Duty;
+
+import java.time.LocalDate;
+
+@Getter
+@RequiredArgsConstructor
+public class UserListItem {
+    private final Long userId;
+    private final String name;
+    private final Sex sex;
+    private final Integer grade;
+    private final String phoneNumber;
+    private final LocalDate birth;
+    private final Duty duty;
+    private final String leaderName;
+}
