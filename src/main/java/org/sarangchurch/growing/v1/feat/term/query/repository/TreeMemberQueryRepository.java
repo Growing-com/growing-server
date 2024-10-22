2eecc6b7ddf9ce9361021dc22b0ba972ebbc5305
sanguk <imcool2551@gmail.com>
2024-10-22 17:38:58 +0900

    feat: query codies by term and its leaders

--- src/main/java/org/sarangchurch/growing/v1/feat/term/query/repository/TreeMemberQueryRepository.java
+++ src/main/java/org/sarangchurch/growing/v1/feat/term/query/repository/TreeMemberQueryRepository.java
@@ -1,8 +1,12 @@
 package org.sarangchurch.growing.v1.feat.term.query.repository;
 
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.sarangchurch.growing.v1.feat.term.query.model.CodyListItem;
+import org.sarangchurch.growing.v1.feat.term.query.model.GroupListItem;
+import org.sarangchurch.growing.v1.feat.term.query.model.GroupType;
 import org.sarangchurch.growing.v1.feat.term.query.model.TreeMemberListItem;
 import org.sarangchurch.growing.v1.feat.user.domain.user.QUser;
 import org.springframework.stereotype.Repository;
@@ -14,6 +18,7 @@ import java.util.List;
 import static org.sarangchurch.growing.v1.feat.newfamily.domain.newfamilygroup.QNewFamilyGroup.newFamilyGroup;
 import static org.sarangchurch.growing.v1.feat.newfamily.domain.newfamilygroupleader.QNewFamilyGroupLeader.newFamilyGroupLeader;
 import static org.sarangchurch.growing.v1.feat.newfamily.domain.newfamilygroupmember.QNewFamilyGroupMember.newFamilyGroupMember;
+import static org.sarangchurch.growing.v1.feat.term.domain.cody.QCody.cody;
 import static org.sarangchurch.growing.v1.feat.term.domain.smallgroup.QSmallGroup.smallGroup;
 import static org.sarangchurch.growing.v1.feat.term.domain.smallgroupleader.QSmallGroupLeader.smallGroupLeader;
 import static org.sarangchurch.growing.v1.feat.term.domain.smallgroupmember.QSmallGroupMember.smallGroupMember;
@@ -23,6 +28,16 @@ import static org.sarangchurch.growing.v1.feat.user.domain.user.QUser.user;
 @RequiredArgsConstructor
 public class TreeMemberQueryRepository {
     private final JPAQueryFactory queryFactory;
+
+    public List<CodyListItem> findCodiesByTerm(Long termId) {
+        return queryFactory.select(Projections.constructor(CodyListItem.class,
+                        cody.id.as("codyId"),
+                        user.name.as("codyName")
+                ))
+                .from(cody)
+                .join(user).on(user.id.eq(cody.userId), cody.termId.eq(termId))
+                .fetch();
+    }
 
     public List<TreeMemberListItem> findBySmallGroup(Long smallGroupId) {
         QUser leaderUser = new QUser("leaderUser");
@@ -146,6 +161,37 @@ public class TreeMemberQueryRepository {
         result.addAll(result4);
 
         result.sort(Comparator.comparing(TreeMemberListItem::getLeaderName));
+
+        return result;
+    }
+
+    public List<GroupListItem> findGroupListByCody(Long codyId) {
+        List<GroupListItem> smallGroups = queryFactory.select(Projections.constructor(GroupListItem.class,
+                        smallGroup.id.as("groupId"),
+                        user.name.as("leaderName"),
+                        Expressions.asEnum(GroupType.SMALL_GROUP).as("groupType")
+                ))
+                .from(smallGroup)
+                .join(cody).on(cody.id.eq(smallGroup.codyId), cody.id.eq(codyId))
+                .join(smallGroupLeader).on(smallGroupLeader.id.eq(smallGroup.smallGroupLeaderId))
+                .join(user).on(user.id.eq(smallGroupLeader.userId))
+                .fetch();
+
+        List<GroupListItem> newFamilyGroups = queryFactory.select(Projections.constructor(GroupListItem.class,
+                        newFamilyGroup.id.as("groupId"),
+                        user.name.as("leaderName"),
+                        Expressions.asEnum(GroupType.NEW_FAMILY_GROUP).as("groupType")
+                ))
+                .from(newFamilyGroup)
+                .join(cody).on(cody.id.eq(newFamilyGroup.codyId), cody.id.eq(codyId))
+                .join(newFamilyGroupLeader).on(newFamilyGroupLeader.id.eq(newFamilyGroup.newFamilyGroupLeaderId))
+                .join(user).on(user.id.eq(newFamilyGroupLeader.userId))
+                .fetch();
+
+        List<GroupListItem> result = new ArrayList<>();
+
+        result.addAll(smallGroups);
+        result.addAll(newFamilyGroups);
 
         return result;
     }
