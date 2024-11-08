184b8b9b920599aee0dbf679eecb5e18e26bbc13
sanguk <imcool2551@gmail.com>
2024-11-08 13:22:40 +0900

    feat: lineup small grp leader

--- src/main/java/org/sarangchurch/growing/v1/feat/lineup/domain/smallgroupleaderlineup/SmallGroupLeaderLineUpRepository.java
+++ src/main/java/org/sarangchurch/growing/v1/feat/lineup/domain/smallgroupleaderlineup/SmallGroupLeaderLineUpRepository.java
@@ -1,5 +1,11 @@
 package org.sarangchurch.growing.v1.feat.lineup.domain.smallgroupleaderlineup;
 
+import java.util.List;
+
 public interface SmallGroupLeaderLineUpRepository {
     boolean existsByLeaderUserIdAndTermId(Long userId, Long termId);
+
+    <S extends SmallGroupLeaderLineUp> List<S> saveAll(Iterable<S> smallGroupLeaderLineUps);
+
+    void deleteByCodyUserIdAndTermId(Long codyId, Long termId);
 }
