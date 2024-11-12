d390e30e9f532d27bcbfad0db96b96adccacdeed
sanguk <imcool2551@gmail.com>
2024-11-12 14:47:24 +0900

    feat: add cody line up delete validation

--- src/main/java/org/sarangchurch/growing/v1/feat/lineup/infra/component/CodyAssigner.java
+++ src/main/java/org/sarangchurch/growing/v1/feat/lineup/infra/component/CodyAssigner.java
@@ -1,7 +1,11 @@
 package org.sarangchurch.growing.v1.feat.lineup.infra.component;
 
 import lombok.RequiredArgsConstructor;
+import org.sarangchurch.growing.v1.feat.lineup.domain.newfamilygroupleaderlineup.NewFamilyGroupLeaderLineUp;
+import org.sarangchurch.growing.v1.feat.lineup.domain.smallgroupleaderlineup.SmallGroupLeaderLineUp;
 import org.sarangchurch.growing.v1.feat.lineup.domain.stumplineup.StumpLineUp;
+import org.sarangchurch.growing.v1.feat.lineup.infra.data.newfamilygroupleaderlineup.NewFamilyGroupLeaderLineUpReader;
+import org.sarangchurch.growing.v1.feat.lineup.infra.data.smallgroupleaderlineup.SmallGroupLeaderLineUpReader;
 import org.sarangchurch.growing.v1.feat.lineup.infra.data.stumplineup.StumpLineUpFinder;
 import org.sarangchurch.growing.v1.feat.lineup.infra.data.stumplineup.StumpLineUpWriter;
 import org.sarangchurch.growing.v1.feat.term.domain.term.Term;
@@ -10,16 +14,19 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class CodyAssigner {
     private final StumpLineUpFinder stumpLineUpFinder;
     private final StumpLineUpWriter stumpLineUpWriter;
+    private final SmallGroupLeaderLineUpReader smallGroupLeaderLineUpReader;
+    private final NewFamilyGroupLeaderLineUpReader newFamilyGroupLeaderLineUpReader;
     private final NormalLineUpAvailableValidator normalLineUpAvailableValidator;
 
     @Transactional
-    public void assign(Term term, List<User> users) {
+    public void assign(Term term, List<User> codyUsers) {
         StumpLineUp stumpLineUp = stumpLineUpFinder.findByTermId(term.getId())
                 .orElseGet(() ->
                         stumpLineUpWriter.save(
@@ -29,8 +36,36 @@ public class CodyAssigner {
                         )
                 );
 
-        users.forEach(user -> normalLineUpAvailableValidator.validate(term, user));
+        validateAssignedLeaderExists(term, codyUsers);
 
-        stumpLineUp.setCodies(users);
+        codyUsers.forEach(user -> normalLineUpAvailableValidator.validate(term, user));
+
+        stumpLineUp.setCodies(codyUsers);
+    }
+
+    private void validateAssignedLeaderExists(Term term, List<User> codyUsers) {
+        List<SmallGroupLeaderLineUp> smallGroupLeaderLineUps = smallGroupLeaderLineUpReader.findByTermId(term.getId());
+
+        for (SmallGroupLeaderLineUp smallGroupLeaderLineUp : smallGroupLeaderLineUps) {
+            Optional<User> codyUserOptional = codyUsers.stream()
+                    .filter(it -> it.getId().equals(smallGroupLeaderLineUp.getCodyUserId()))
+                    .findAny();
+
+            if (codyUserOptional.isEmpty()) {
+                throw new IllegalStateException("담당하는 일반 순장이 있는 코디는 삭제할 수 없습니다.");
+            }
+        }
+
+        List<NewFamilyGroupLeaderLineUp> newFamilyGroupLeaderLineUps = newFamilyGroupLeaderLineUpReader.findByTermId(term.getId());
+
+        for (NewFamilyGroupLeaderLineUp newFamilyGroupLeaderLineUp : newFamilyGroupLeaderLineUps) {
+            Optional<User> codyUserOptional = codyUsers.stream()
+                    .filter(it -> it.getId().equals(newFamilyGroupLeaderLineUp.getCodyUserId()))
+                    .findAny();
+
+            if (codyUserOptional.isEmpty()) {
+                throw new IllegalStateException("담당하는 새가족 순장이 있는 코디는 삭제할 수 없습니다.");
+            }
+        }
     }
 }
