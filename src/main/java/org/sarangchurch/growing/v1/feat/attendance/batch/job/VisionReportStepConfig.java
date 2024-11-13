c4836ee9ad7af4f4bf7137bf2b8c62fcbd8710a6
sanguk <imcool2551@gmail.com>
2024-11-13 17:49:15 +0900

    refactor: cleanup

--- src/main/java/org/sarangchurch/growing/v1/feat/attendance/batch/job/VisionReportStepConfig.java
+++ src/main/java/org/sarangchurch/growing/v1/feat/attendance/batch/job/VisionReportStepConfig.java
@@ -19,6 +19,8 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDate;
+
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
@@ -42,30 +44,29 @@ public class VisionReportStepConfig {
         return stepBuilderFactory.get(STEP_NAME)
                 .tasklet((contribution, chunkContext) -> {
                     Sunday sunday = jobParameter.getSunday();
-
-                    log.info("week: {}", sunday);
+                    LocalDate sundayDate = sunday.getDate();
 
                     long activeUserCount = userDownstream.countActiveUsers();
 
-                    long attendanceCount = attendanceReader.countByDate(sunday.getDate());
-                    long stumpAttendanceCount = stumpAttendanceReader.countByDate(sunday.getDate());
-                    long newFamilyAttendanceCount = newFamilyAttendanceReader.countByDate(sunday.getDate());
+                    long attendanceCount = attendanceReader.countByDate(sundayDate);
+                    long stumpAttendanceCount = stumpAttendanceReader.countByDate(sundayDate);
+                    long newFamilyAttendanceCount = newFamilyAttendanceReader.countByDate(sundayDate);
+
                     long totalAttendanceRegistered = attendanceCount + stumpAttendanceCount + newFamilyAttendanceCount;
                     double attendanceRegisterRate = (double) totalAttendanceRegistered / activeUserCount;
 
                     if (attendanceRegisterRate < 0.9d) {
                         contribution.setExitStatus(new ExitStatus("FAIL", "출석율이 90% 이상이어야 갱신할 수 있습니다. 현재: " + attendanceRegisterRate));
                     } else {
-                        contribution.setExitStatus(new ExitStatus("SUCCESS", "비전 리포트가 갱신되었습니다. 날짜: " + sunday.getDate().toString()));
+                        contribution.setExitStatus(new ExitStatus("SUCCESS", "비전 리포트가 갱신되었습니다. 날짜: " + sundayDate.toString()));
 
-                        VisionReport visionReport = visionReportReader.findByDate(sunday.getDate())
-                                .orElseGet(() ->
-                                        visionReportWriter.save(
-                                                VisionReport.builder()
-                                                        .date(sunday.getDate())
-                                                        .totalActive((int) activeUserCount)
-                                                        .totalAttendanceRegistered((int) totalAttendanceRegistered)
-                                                        .build())
+                        VisionReport visionReport = visionReportReader.findByDate(sundayDate)
+                                .orElse(
+                                        VisionReport.builder()
+                                                .date(sundayDate)
+                                                .totalActive((int) activeUserCount)
+                                                .totalAttendanceRegistered((int) totalAttendanceRegistered)
+                                                .build()
                                 );
 
                         visionReport.update(activeUserCount, totalAttendanceRegistered);
