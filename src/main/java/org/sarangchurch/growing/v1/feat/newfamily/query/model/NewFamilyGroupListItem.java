2060124cf60df4d7b69d2d044d278bc437e845b2
sanguk <imcool2551@gmail.com>
2024-11-13 20:12:49 +0900

    feat: add sex and grade key on 3 endpoints

--- src/main/java/org/sarangchurch/growing/v1/feat/newfamily/query/model/NewFamilyGroupListItem.java
+++ src/main/java/org/sarangchurch/growing/v1/feat/newfamily/query/model/NewFamilyGroupListItem.java
@@ -2,10 +2,13 @@ package org.sarangchurch.growing.v1.feat.newfamily.query.model;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.sarangchurch.growing.core.interfaces.common.types.Sex;
 
 @Getter
 @RequiredArgsConstructor
 public class NewFamilyGroupListItem {
     private final Long newFamilyGroupId;
     private final String newFamilyGroupLeaderName;
+    private final Sex sex;
+    private final Integer grade;
 }
