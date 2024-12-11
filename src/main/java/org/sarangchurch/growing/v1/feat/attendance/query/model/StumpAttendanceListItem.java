5db39b6c8f0d35f5a83b24ed5557a044bb9bbd8f
sanguk <imcool2551@gmail.com>
2024-12-11 15:07:54 +0900

    feat: query stump attendance by single date

---
+++ src/main/java/org/sarangchurch/growing/v1/feat/attendance/query/model/StumpAttendanceListItem.java
@@ -0,0 +1,37 @@
+package org.sarangchurch.growing.v1.feat.attendance.query.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.sarangchurch.growing.core.interfaces.common.types.Sex;
+import org.sarangchurch.growing.v1.feat.attendance.domain.AttendanceStatus;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+public class StumpAttendanceListItem {
+    private final Long userId;
+    private final String name;
+    private final Sex sex;
+    private final Integer grade;
+    private final List<StumpAttendanceListItemAttendItem> attendanceItems;
+
+    @Getter
+    @RequiredArgsConstructor
+    public static class StumpAttendanceListItemAttendItem {
+        @JsonIgnore
+        private final Long userId;
+        @JsonIgnore
+        private final String name;
+        @JsonIgnore
+        private final Sex sex;
+        @JsonIgnore
+        private final Integer grade;
+
+        private final AttendanceStatus status;
+        private final LocalDate date;
+        private final String reason;
+    }
+}
